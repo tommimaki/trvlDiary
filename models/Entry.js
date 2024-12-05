8029d0e43233731a0d10802d879294f06a4d80c8
Tommi Mäki <bgp010@myy.haaga-helia.fi>
2024-12-05 08:25:03 +0200

    entryschema added

---
+++ models/Entry.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const EntrySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  notes: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Entry || mongoose.model("Entry", EntrySchema);
